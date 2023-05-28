29c46d9dc9f1654ba24e47ade1f02b8f30269df0
Anastasiya <ephimova.ad@gmail.com>
2023-05-29 00:29:58 +0400

    Layout adapted for mobile

--- src/components/common/PlugComponent.vue
+++ src/components/common/PlugComponent.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="text-center text-h4 primary--text mt-8">
     <div class="blue-grey--text text--darken-3">Here is empty</div>
-    <div><img src="@/assets/cat.png" height="600px" /></div>
+    <div>
+      <img src="@/assets/cat.png" :height="isMobile ? '250px' : '600px'" />
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    isMobile() {
+      return this.$vuetify.breakpoint.smAndDown;
+    },
+  },
+};
+</script>
